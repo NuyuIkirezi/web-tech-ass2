2c15d66b090fd8a441161b627bd108def97a8d43
afanyu <afanyuemma2002@gmail.com>
2026-02-02 22:40:33 +0200

    correction

---
+++ .history/src/main/java/com/nuyu/servlets/SearchRedirectServlet_20260202223952.java
@@ -0,0 +1,24 @@
+package com.nuyu.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SearchRedirectServlet extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+       
+        String searchQuery = request.getParameter("searchQuery");
+        
+
+        String encodedQuery = java.net.URLEncoder.encode(searchQuery, "UTF-8");
+        
+        response.sendRedirect("https://www.google.com/search?q=" + encodedQuery);
+    }
+}
